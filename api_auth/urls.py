12094ee9c6ed2abffdb31d876f6938e186ec1dd5
Ivan <reznikov.ivan@outlook.com>
2021-06-02 11:20:57 +0300

    Created User model, serializer, paths, emailsignup viewset

---
+++ api_auth/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('auth/email/', views.SignUpEmail.as_view()),
+    #path('users/me/', views.UserProfile.as_view()),
+    path('', include(router.urls)),
+]
